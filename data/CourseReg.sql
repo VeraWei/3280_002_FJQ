6d0ed1ee361493abfa575dc08d7abe9f2338fa03
Joel Giladi <joel@giladi.dev>
2020-07-19 03:01:51 -0700

    changed database to resemble original design for simplified purposes. Also added draw.io file for reference

--- data/CourseReg.sql
+++ data/CourseReg.sql
@@ -3,66 +3,99 @@ CREATE DATABASE Registrar;
 USE Registrar;
 
 -- create table STUDENTS
-create table STUDENTS (
-	StudentID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE STUDENTS (
+	StudentID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	FName CHAR(20) NOT NULL,
-    Sex CHAR(1) NOT NULL,
-    PhoneNumber INT NOT NULL,
-    Email VARCHAR (35) NOT NULL,
-    StudentType CHAR(2) NULL
+	LName CHAR(20) NOT NULL,
+    PhoneNumber Char(18) NOT NULL,
+    Email VARCHAR (35) NOT NULL
 
 ) Engine=InnoDB;
 
-insert into STUDENTS (FacilityName, description) values ('Meeting Room', 'A small room with access to internet and power outlet. Maximum occupation is 10 persons');
-insert into STUDENTS (FacilityName, description) values ('Classroom', 'A normal classroom with access to internet, power outlet, presenter desktop and projector. Maximum occupation is 50 persons');
-insert into STUDENTS (FacilityName, description) values ('High-Tech Room', 'A high tech classroom with access to separate wireless hub, smart power outlet, smart light, audio, high resolution projector, air purifier and security. Maximum occupation is 25 persons');
-insert into STUDENTS (FacilityName, description) values ('Auditorium', 'A big room for seminar with wide screen for presentation, AV recording, and multiple entrance/access. Maximum occupation is 150 persons');
-insert into STUDENTS (FacilityName, description) values ('Party Room', 'A room with a few kitchen utensils, a small fridge and dish sink. Maximum occupation is 50 persons');
+INSERT INTO STUDENTS VALUES (NULL, 'Orlando', 'Bloom', '(778)-(888)-(2233)', 'John.Bloom@college.ca');
+INSERT INTO STUDENTS VALUES (NULL, 'Sarah', 'Connor', '(604)-(777)-(2244)', 'Sarah.Connor@college.ca');
+INSERT INTO STUDENTS VALUES (NULL, 'Bruce', 'Wayne', '(604)-(666)-(2255)', 'Bruce.Wayne@college.ca');
+INSERT INTO STUDENTS VALUES (NULL, 'Larah', 'Croft', '(778)-(555)-(2266)', 'Larah.Croft@college.ca');
+
+
+
+CREATE TABLE INSTRUCTORS (
+	InstructorID INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	InstructorName CHAR(35)	NOT NULL,
+	Phone CHAR(25) NOT NULL,
+	Email VARCHAR(35) NOT NULL
+) Engine=InnoDB;
 
 -- create table COURSES
-create table COURSES (
-	CRN SMALLINT(5) AUTO_INCREMENT PRIMARY KEY,
-	Credits Int(1),
-	Subject VARCHAR(35),
-	Title CHAR(25),
-	InstructorID INT,
-    -- CHANGED PREREQUISITES TO PREREQ (Joel)
-	Prereq BIT NOT NULL DEFAULT 0,
-    -- WHAT ARE SESSION NOTES? (Joel)
-    --CHANGED TAKING TIME TO COURSE DURATION (Joel)
-	CourseDuration TIME NOT NULL,
-	FOREIGN KEY (InstructorID) REFERENCES INSTRUCTOR (InstructorID) ON DELETE NO ACTION ON UPDATE CASCADE
+CREATE TABLE COURSES (
+	CRN SMALLINT(5) NOT NULL PRIMARY KEY,
+	Credits Int(1)	NOT NULL,
+	Subject VARCHAR(35)	NOT NULL,
+	Title CHAR(150)	NOT NULL,
+	InstructorID INT NULL,
+    PreReq BIT NOT NULL DEFAULT 0,
+	Duration INT NOT NULL,
+	Enrl INT(2) NOT NULL,
+    Rem INT(2) NOT NULL,
+    Wait INT(2) NOT NULL,
+	FOREIGN KEY (InstructorID) REFERENCES INSTRUCTORS(InstructorID) ON UPDATE CASCADE ON DELETE NO ACTION
 ) Engine=InnoDB;
 
+--create table TUITION
+CREATE TABLE TUITION (
+	StudentID INT NOT NULL PRIMARY KEY,
+	AmountOwing INT NULL,
+	TuitionPaid BIT NULL,
+	FOREIGN KEY (StudentID) REFERENCES STUDENTS(StudentID) ON UPDATE CASCADE ON DELETE NO ACTION
+) Engine=InnoDB;
+
+--create intersection table STUDENTS_COURSES
+CREATE TABLE STUDENTS_COURSES (
+	CRN SMALLINT(5) NOT NULL,
+	StudentID INT NOT NULL,
+	RegistrationDate Date NOT NULL,
+	PRIMARY KEY(CRN, StudentID),
+	FOREIGN KEY(CRN) REFERENCES COURSES(CRN) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(StudentID) REFERENCES STUDENTS(StudentID) ON UPDATE CASCADE ON DELETE CASCADE
+
+) Engine=InnoDB;
+
+--insert dummy values
+INSERT INTO STUDENTS VALUES (NULL, 'Orlando', 'Bloom', '(778)-(888)-(2233)', 'John.Bloom@college.ca');
+INSERT INTO STUDENTS VALUES (NULL, 'Sarah', 'Connor', '(604)-(777)-(2244)', 'Sarah.Connor@college.ca');
+INSERT INTO STUDENTS VALUES (NULL, 'Bruce', 'Wayne', '(604)-(666)-(2255)', 'Bruce.Wayne@college.ca');
+INSERT INTO STUDENTS VALUES (NULL, 'Larah', 'Croft', '(778)-(555)-(2266)', 'Larah.Croft@college.ca');
+
+INSERT INTO INSTRUCTORS VALUES (NULL, 'Bruce Willis', '(778)-(600)-(1120)', 'Bruce.Willis@instructor.ca');
+INSERT INTO INSTRUCTORS VALUES (NULL, 'Leon Kepling', '(778)-(600)-(1121)', 'Leon Kepling@instructor.ca');
+INSERT INTO INSTRUCTORS VALUES (NULL, 'James Bond', '(778)-(600)-(1122)', 'James.Bond@instructor.ca');
+INSERT INTO INSTRUCTORS VALUES (NULL, 'Jennifer Kent', '(778)-(600)-(1123)', 'Jennifer.Kent@instructor.ca');
+
+INSERT INTO COURSES VALUES(1110, 3, 'CSIS', "Introduction to programming", 1, 0, 45, 2, 23, 0);
+INSERT INTO COURSES VALUES(1280, 3, 'CSIS', "WebScripting 101", 2, 1, 45, 2, 23, 0);
+INSERT INTO COURSES VALUES(2200, 3, 'CSIS', "Database Systems and Design", 3, 0, 45, 0, 25, 0);
+INSERT INTO COURSES VALUES(3300, 3, 'CSIS', "Advanced Database Systems", 4, 1, 45, 0, 25, 0);
+
+INSERT INTO TUITION VALUES(1, 300, 0);
+INSERT INTO TUITION VALUES(2, 200, 0);
+INSERT INTO TUITION VALUES(3, 100, 0);
+INSERT INTO TUITION VALUES(4, 0, 1);
+
+INSERT INTO STUDENTS_COURSES VALUES (1110, 1, '2020-09-02');
+INSERT INTO STUDENTS_COURSES VALUES (1280, 2, '2020-09-02');
+INSERT INTO STUDENTS_COURSES VALUES (2200, 3, '2020-09-02');
+INSERT INTO STUDENTS_COURSES VALUES (3300, 4, '2020-09-02');
+
+
+
+
+
+
+
+SELECT * FROM STUDENTS;
+
+SELECT * FROM COURSES;
+
+SELECT * FROM INSTRUCTORS;
+
 -- STOPPED CODING HERE (Joel)
-
--- insert into reservation table
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (1, 'Collete Brownlee', 'cbrownlee0@mit.edu', 3, '2020-11-07', 7, 'Class Discussion');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (2, 'Obediah Dupree', 'odupree1@addtoany.com', 3, '2020-09-25', 5, 'Midterm Study');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (3, 'Joane Gouldstraw', 'jgouldstraw2@apache.org', 1, '2020-12-13', 7, 'Class Discussion');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (4, 'Chickie Trueman', 'ctrueman3@berkeley.edu', 2, '2020-10-15', 2, 'Midterm Study');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (5, 'Juliann Goslin', 'jgoslin4@mayoclinic.com', 1, '2020-11-13', 5, 'Tutorial');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (6, 'Conchita Flowith', 'cflowith5@github.com', 4, '2020-09-20', 4, 'Project presentation');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (7, 'Nora Teaser', 'nteaser6@ifeng.com', 5, '2020-10-11', 3, 'Midterm Study');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (8, 'Adena Gonnin', 'agonnin7@shareasale.com', 4, '2020-08-09', 1, 'Tutorial');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (9, 'Hyacinthe Fisby', 'hfisby8@istockphoto.com', 1, '2020-11-02', 3, 'Project presentation');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (10, 'Emili Middle', 'emiddle9@europa.eu', 1, '2020-12-03', 3, 'Project presentation');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (11, 'Goldarina Hark', 'gharka@jigsy.com', 5, '2020-09-06', 5, 'Project Meeting');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (12, 'Evy Hawthorn', 'ehawthornb@yahoo.co.jp', 4, '2020-08-23', 2, 'Graduation Party');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (13, 'Lurleen Standering', 'lstanderingc@vimeo.com', 4, '2020-10-24', 7, 'Project Meeting');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (14, 'Maxie Allgood', 'mallgoodd@i2i.jp', 2, '2020-12-14', 1, 'Project presentation');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (15, 'Meaghan Dufour', 'mdufoure@miibeian.gov.cn', 3, '2020-09-20', 5, 'Tutorial');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (16, 'Marwin Whitman', 'mwhitmanf@posterous.com', 5, '2020-07-14', 3, 'Tutorial');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (17, 'Luther Grogona', 'lgrogonag@chicagotribune.com', 5, '2020-10-20', 1, 'Class Discussion');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (18, 'Ruben McCoid', 'rmccoidh@illinois.edu', 1, '2020-10-13', 1, 'Graduation Party');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (19, 'Amy Simmell', 'asimmelli@cloudflare.com', 4, '2020-12-17', 2, 'Project presentation');
-insert into reservation (ReservationID, name, email, FacilityID, ReservationDate , Length, Venue) values (20, 'Darcee Garbutt', 'dgarbuttj@usgs.gov', 3, '2020-09-12', 5, 'Midterm Study');
-
-DESC facility; 
-SELECT COUNT(*) FROM facility; -- 5
-SELECT * FROM facility;
-
-DESC reservation;
-SELECT COUNT(*) FROM reservation; -- 20
-SELECT * FROM reservation;
-
