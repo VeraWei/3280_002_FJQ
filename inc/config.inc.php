d65547425c8456b342e7267aa3a0dc92a91b54d5
Fernando Maia <fvianamaia@gmail.com>
2020-07-26 19:47:57 -0700

    inc/config.inc.php adjusted to include dbconfig.php as well.

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+if (file_exists("dbconfig.inc.php")) {
+    require_once("dbconfig.inc.php");
+}
 // definition for log file
 define('LOGFILE','log/error_log.txt');
 ini_set("log_errors", TRUE);  
